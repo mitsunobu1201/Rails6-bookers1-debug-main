e60b21b58eccf47b50a5027eede89c4eba6c59f9
mitsunobu1201 <87081727+mitsunobu1201@users.noreply.github.com>
2023-02-24 19:12:01 +0000

    [Add]titleテーブル追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   root to: "homes#top"
-  resources :books, only: [:index,:show]
+  resources :books, only: [:index,:show,:create,:edit,:update,:destroy]
 end
